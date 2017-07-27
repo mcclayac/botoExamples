5f1c6a302879fe6c5a7278b706752a6a303f5bfc
amcclay <anthony.mcclay@me.com>
2017-07-27 02:59:35 -0400

    S3 Examples

---
+++ S3examples/listBuckets.py
@@ -0,0 +1,21 @@
+
+
+import boto3
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Call S3 to list current buckets
+response = s3.list_buckets()
+
+# Get a list of all bucket names from the response
+buckets = [bucket['Name'] for bucket in response['Buckets']]
+
+# Print out the bucket list
+print("Bucket List: %s" % buckets)
+
+
+# Create an S3 client
+for row in response['Buckets']:
+        print(row['Name'])
+
